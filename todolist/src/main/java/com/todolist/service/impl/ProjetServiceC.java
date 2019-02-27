a651ad5e0036305c903634e0624e87699f68e21e
Mononoke76 <c.teillet@yahoo.fr>
2019-02-27 19:51:56 +0100

    ajout de la methode findAll() pour avoir la liste des projets d'un utilisateur

--- todolist/src/main/java/com/todolist/service/impl/ProjetServiceC.java
+++ todolist/src/main/java/com/todolist/service/impl/ProjetServiceC.java
@@ -1,7 +1,7 @@
 package com.todolist.service.impl;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import javax.transaction.Transactional;
 
@@ -43,6 +43,15 @@ public class ProjetServiceC implements IProjetServiceC{
 		
 		return projetDto;
 	}
+
+	@Override
+	public List<ProjetDtoC> findAll(Long idUtilisateur) {
+		List<Projet> projets=  utilisateurRepository.findById(idUtilisateur).get().getProjets();
+		List<ProjetDtoC> projetsDto= projets.stream().map(projet -> new ProjetDtoC(projet)).collect(Collectors.toList());
+		return projetsDto;
+	}
+	
+	
 	
 
 }
--- todolist/src/main/java/com/todolist/service/impl/TacheService.java
+++ todolist/src/main/java/com/todolist/service/impl/TacheService.java
@@ -1,6 +1,5 @@
 package com.todolist.service.impl;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import javax.transaction.Transactional;
