c0e83b4968d978b91dcd2d7e5d2f8dceca8e08d2
maximebn <maximebn@posteo.ru>
2019-03-01 09:49:25 +0100

    Modif création utilisateur : vérification validité mail + modif userdata

--- todolist/src/main/java/com/todolist/service/IUtilisateurService.java
+++ todolist/src/main/java/com/todolist/service/IUtilisateurService.java
@@ -1,9 +1,13 @@
 package com.todolist.service;
 
+import javax.mail.internet.AddressException;
+
 import com.todolist.dto.UtilisateurDto;
 
 public interface IUtilisateurService {
 	
-	UtilisateurDto save(UtilisateurDto userDto);
+	UtilisateurDto register(UtilisateurDto userDto) throws AddressException;
+
+	UtilisateurDto updateUserData(UtilisateurDto userDto, long idUtilisateur) throws AddressException;
 
 }
